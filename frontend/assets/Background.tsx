80b8e8829fc3401d9ca48310ce9978c0ef48a9b9
mpvilamarin <mpvillamarin@gmail.com>
2023-11-02 17:26:05 -0300

    ajustes de escenario

---
+++ frontend/assets/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Background" tilewidth="6225" tileheight="970" tilecount="1" columns="1">
+ <image source="../../../../../Desktop/Escenarios.png" width="6225" height="970"/>
+</tileset>
